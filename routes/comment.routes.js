9f1cf8931098068ea5a728af05e83ddf35ea83cd
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-08-05 22:14:06 +0100

    Added a check if the commenter is not the creator of the service

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const User = require('../models/User.model');
 const Comment = require('../models/Comment.model');
 const Transaction = require('../models/Transaction.model');
+const Service = require('../models/Service.model')
+
+
+// Create a comment only if the user has already bought the product
 
 router.post('/comment', async (req, res, next) => {
 	try {
@@ -10,12 +14,17 @@ router.post('/comment', async (req, res, next) => {
 		// Find all transactions of the user
 		const userTransactions = await Transaction.find(userId);
 
+		
+		// Find the userId of the person who created the service(so that sellers cannot comment their own services)
+		const sellerId = await Service.findById(serviceId).populate('createdBy')
+
+		console.log('Seller ID is: ', sellerId) // ??? why is it showing the whole service ?
         
 
-        console.log(userTransactions.find(item => item.cart.filter(service => service.serviceId.toString() === serviceId).length))
+        //console.log(userTransactions.find(item => item.cart.filter(service => service.serviceId.toString() === serviceId).length))
 
-		// Check if the user has already bought the service
-		if (userTransactions.find(item => item.cart.filter(service => service.serviceId.toString() === serviceId).length)) {
+		// Check if the user has already bought the service and if they're not the creator of the service
+		if (userTransactions.find(item => item.cart.filter(service => service.serviceId.toString() === serviceId).length) && sellerId.toString() !== userId ) {
             
 			const newComment = await Comment.create({
 				serviceId,
@@ -40,5 +49,12 @@ router.post('/comment', async (req, res, next) => {
 		console.error(error);
 	}
 });
+
+// Edit the comment posted by the user 
+
+
+
+
+
 
 module.exports = router;
