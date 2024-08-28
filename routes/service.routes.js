a97e767c172d609920e415542970ddcf544649cb
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-08-28 15:01:26 +0100

    Desactivate added

--- routes/service.routes.js
+++ routes/service.routes.js
@@ -13,7 +13,7 @@ router.post('/services', async (req, res, next) => {
 			date,
 			img,
 			category,
-			createdBy
+			createdBy,
 		} = req.body;
 
 		// create a new service
@@ -26,7 +26,7 @@ router.post('/services', async (req, res, next) => {
 			img,
 			category,
 			createdBy,
-			isActive: quantity > 0
+			isActive: quantity > 0,
 		});
 
 		// add the service offered to the user
@@ -46,7 +46,7 @@ router.post('/services', async (req, res, next) => {
 
 router.get('/services', async (req, res, next) => {
 	try {
-		const activeServices = await Service.find({isActive: true});
+		const activeServices = await Service.find({ isActive: true });
 
 		res.status(200).json(activeServices);
 	} catch (error) {
@@ -72,8 +72,15 @@ router.get('/services/:serviceId', async (req, res, next) => {
 router.put('/services/edit/:serviceId', async (req, res, next) => {
 	try {
 		const { serviceId } = req.params;
-		const { serviceName, serviceDescription, quantity, price, date, img, category } =
-			req.body;
+		const {
+			serviceName,
+			serviceDescription,
+			quantity,
+			price,
+			date,
+			img,
+			category,
+		} = req.body;
 
 		const updatedService = await Service.findByIdAndUpdate(
 			serviceId,
@@ -104,17 +111,36 @@ router.delete('/services/:serviceId', async (req, res, next) => {
 		// Delete the service by its ID
 		const serviceDeletion = await Service.findByIdAndDelete(serviceId);
 
-
-		// Find the user with the serviceId in their servicesOffered array and remove it 
-		const userUpdate = await User.findByIdAndUpdate(
-			serviceDeletion.createdBy,
-			{ $pull: { servicesOffered: serviceId } },
-		);
+		// Find the user with the serviceId in their servicesOffered array and remove it
+		const userUpdate = await User.findByIdAndUpdate(serviceDeletion.createdBy, {
+			$pull: { servicesOffered: serviceId },
+		});
 
 		// Run both operations simultaneously
 		//await Promise.all([serviceDeletion, userUpdate])
 
 		res.status(204).send();
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+// Desactivate
+
+router.put('/services/:serviceId', async (req, res, next) => {
+	try {
+		const { serviceId } = req.params;
+
+		// Find service by Id and make it inactive
+		const { isActive } = req.body;
+
+		const desactivateService = await Service.findByIdAndUpdate(
+			serviceId,
+			{ isActive: false },
+			{ new: true },
+		);
+
+		res.status(200).json(desactivateService);
 	} catch (error) {
 		console.error(error);
 	}
